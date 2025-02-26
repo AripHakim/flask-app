4f027de03dc94879f7a5ef360f566c660b117699
Arief <93286382+AripHakim@users.noreply.github.com>
2025-02-27 06:49:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ app = Flask(__name__)
 CORS(app, resources={r"/plagiarism": {"origins": "https://winnowing-web.vercel.app"}},
      supports_credentials=True)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
 def winnowing_fingerprint(text, k, window_size):
     shingles = [text[i:i+k] for i in range(len(text) - k + 1)]
     hashes = [hashlib.md5(shingle.encode('utf-8')).hexdigest() for shingle in shingles]
@@ -52,3 +49,7 @@ def detect_plagiarism():
             })
     
     return jsonify({'similarities': similarities})
+
+     
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
